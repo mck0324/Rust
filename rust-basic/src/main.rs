1f31dc965be5cc64f7119e7925932bd45cf40bd2
mck0324 <dch3030@naver.com>
2024-09-21 20:31:34 +0900

    Use Mecro Fucntion

--- rust-basic/src/main.rs
+++ rust-basic/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    println!("Number: {}, String: {}", 100, "abc");
+    // println!("Number: {}, String: {}", 100, "abc");
+    println!("Weight on Mars: {}kg", calculate_weight_on_mars(100.0));
 
-    calculate_weight_on_mars(100.9);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    return 50.0;
+    (weight / 9.81) * 3.711
 }
