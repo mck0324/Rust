05235316c01adc81b38b3d437cf842c10aced099
mck0324 <dch3030@naver.com>
2024-09-21 21:20:17 +0900

    Rust I/O

--- rust-basic/src/main.rs
+++ rust-basic/src/main.rs
@@ -1,4 +1,8 @@
+use std::io;
+
 fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
     let mut mars_weight = calculate_weight_on_mars(100.0);
     // println!("Number: {}, String: {}", 100, "abc");
     mars_weight = mars_weight * 1000.0;
