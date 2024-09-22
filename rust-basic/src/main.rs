fad00b74698c16cba306fdc7576e188a6b92854f
mck0324 <dch3030@naver.com>
2024-09-23 00:19:52 +0900

    Reference

--- rust-basic/src/main.rs
+++ rust-basic/src/main.rs
@@ -2,11 +2,9 @@ use std::io;
 
 fn main() {
     let mut input = String::new();
-    some_fn(input);
     io:: stdin().read_line(&mut input);
-    
-    let mut mars_weight = calculate_weight_on_mars(100.0);
-    mars_weight = mars_weight * 1000.0;
+    println!("Input: {}", input);
+    let mars_weight = calculate_weight_on_mars(100.0);
     println!("Weight on Mars: {}kg", mars_weight);
 
 }
@@ -14,6 +12,4 @@ fn main() {
 fn calculate_weight_on_mars(weight: f32) -> f32 {
     (weight / 9.81) * 3.711
 }
-
-fn some_fn(s: String) {}
 
