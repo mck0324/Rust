3ff458eb57d06909929a95a6e4b32097e3e563e6
mck0324 <dch3030@naver.com>
2024-09-29 14:46:14 +0900

    module

--- rust-http/src/main.rs
+++ rust-http/src/main.rs
@@ -16,41 +16,49 @@ fn main() {
 
 
 
-    let server = Server::new("127.0.0.1:8080".to_string());
+    let server = server::Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-
-}
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-         }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
-}
-
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method,
-}
-
-enum Method {
-    GET,
-    DELETE,
-    POST,
-    PUT,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
+mod server {
+    pub struct Server {
+        addr: String,
     
+    }
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self {
+                addr
+             }
+        }
+    
+        pub fn run(self) {
+            println!("Listening on {}", self.addr);
+        }
+    }
+}
+
+mod http {
+    mod request {
+        struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: super::method::Method,
+        }
+    }
+    
+    mod method {
+        pub enum Method {
+            GET,
+            DELETE,
+            POST,
+            PUT,
+            HEAD,
+            CONNECT,
+            OPTIONS,
+            TRACE,
+            PATCH,
+            
+        }
+    }
 }
