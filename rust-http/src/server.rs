a395be7a0f6d905b0f23f72a99efd05d850833b8
mck0324 <dch3030@naver.com>
2024-10-01 18:58:58 +0900

    crate/type change

--- rust-http/src/server.rs
+++ rust-http/src/server.rs
@@ -1,4 +1,7 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::{io::Read, net::TcpListener};
+
 
 pub struct Server {
     addr: String,
@@ -20,7 +23,12 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("Receive a requst {}", String::from_utf8_lossy(&buffer));
+                            println!("Receive a request {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..])  {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request {}", e), 
+                            }
                         }
                         Err(e) => println!("Failed to read from connect {}",e),
                         
