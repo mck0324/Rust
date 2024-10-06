84b8e8a1068cb09c035f7b96d4f45ce7ac276372
mck0324 <dch3030@naver.com>
2024-10-06 22:50:37 +0900

    HTTP Response Modeling

--- rust-http/target/debug/deps/rust_http-2b7fd4fa6b7611cb.d
+++ rust-http/target/debug/deps/rust_http-2b7fd4fa6b7611cb.d
@@ -1,6 +1,6 @@
-/Users/minchan/Desktop/study/rust/rust-http/target/debug/deps/librust_http-2b7fd4fa6b7611cb.rmeta: src/main.rs src/server.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/http/query_string.rs
+/Users/minchan/Desktop/study/rust/rust-http/target/debug/deps/librust_http-2b7fd4fa6b7611cb.rmeta: src/main.rs src/server.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/http/query_string.rs src/http/response.rs
 
-/Users/minchan/Desktop/study/rust/rust-http/target/debug/deps/rust_http-2b7fd4fa6b7611cb.d: src/main.rs src/server.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/http/query_string.rs
+/Users/minchan/Desktop/study/rust/rust-http/target/debug/deps/rust_http-2b7fd4fa6b7611cb.d: src/main.rs src/server.rs src/http/mod.rs src/http/method.rs src/http/request.rs src/http/query_string.rs src/http/response.rs
 
 src/main.rs:
 src/server.rs:
@@ -8,3 +8,4 @@ src/http/mod.rs:
 src/http/method.rs:
 src/http/request.rs:
 src/http/query_string.rs:
+src/http/response.rs:
